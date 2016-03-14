e839a83bcf7e67949f3359bcc4ecae69acffa7eb
dosmarek <dstlmrk@gmail.com>
2016-03-14 21:22:54 +0100

    manage database connections correctly

--- catcher/middleware.py
+++ catcher/middleware.py
@@ -3,38 +3,41 @@
 
 import falcon
 import simplejson as json
+import models
+
+class PeeweeConnection(object):
+    def process_request(self, req, resp):
+        models.db.connect()
+
+    def process_response(self, req, resp, resource):
+        if not models.db.is_closed():
+            models.db.close()
 
 class Authorization(object):
-    def process_request(self, req, resp):
-        pass
+    pass
+    # def process_request(self, req, resp):
+    #     token = req.get_header('X-Auth-Token')
+    #     project = req.get_header('X-Project-ID')
 
-# -----------TODO --------------------------------------------------
-# class AuthMiddleware(object):
+    #     if token is None:
+    #         description = ('Please provide an auth token '
+    #                        'as part of the request.')
 
-#     def process_request(self, req, resp):
-#         token = req.get_header('X-Auth-Token')
-#         project = req.get_header('X-Project-ID')
+    #         raise falcon.HTTPUnauthorized('Auth token required',
+    #                                       description,
+    #                                       href='http://docs.example.com/auth')
 
-#         if token is None:
-#             description = ('Please provide an auth token '
-#                            'as part of the request.')
+    #     if not self._token_is_valid(token, project):
+    #         description = ('The provided auth token is not valid. '
+    #                        'Please request a new token and try again.')
 
-#             raise falcon.HTTPUnauthorized('Auth token required',
-#                                           description,
-#                                           href='http://docs.example.com/auth')
+    #         raise falcon.HTTPUnauthorized('Authentication required',
+    #                                       description,
+    #                                       href='http://docs.example.com/auth',
+    #                                       scheme='Token; UUID')
 
-#         if not self._token_is_valid(token, project):
-#             description = ('The provided auth token is not valid. '
-#                            'Please request a new token and try again.')
-
-#             raise falcon.HTTPUnauthorized('Authentication required',
-#                                           description,
-#                                           href='http://docs.example.com/auth',
-#                                           scheme='Token; UUID')
-
-#     def _token_is_valid(self, token, project):
-#         return True  # Suuuuuure it's valid...
-# ------------------------------------------------------------------
+    # def _token_is_valid(self, token, project):
+    #     return True  # Suuuuuure it's valid...
 
 class RequireJSON(object):
     def process_request(self, req, resp):
@@ -75,7 +78,7 @@ class JSONTranslator(object):
                 )
 
     def process_response(self, req, resp, resource):
-        # TODO: v pripade souboru to muze fungovat jinak, musi se kdyztak pouzit
-        # reseni z dokumentace
-        if resp.body is not None:
-            resp.body = json.dumps(resp.body)
+        # vysledky neukladam do req.context, protoze do body se muzou davat jeste jine veci
+        if 'result' not in req.context:
+            return
+        resp.body = json.dumps(req.context['result'])
