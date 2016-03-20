28a1105093982dacb2365a2bf9f58adf6d5a5f2d
dosmarek <dstlmrk@gmail.com>
2016-03-20 14:24:44 +0100

    config files

---
+++ catcher/config/cald.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# coding=utf-8
+
+clubs = dict(
+    url       = 'http://archiv.cald.cz/caldMembersRecord/public/report/teams',
+    # order is important
+    columns   = ['ID oddílu', 'Oddíl', 'ID hráče', 'Příjmení', 'Jméno'],
+    delimiter = '\t'
+)
