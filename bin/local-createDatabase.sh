f482893d1db5930fc13065ff00e4ffa783bcc754
dosmarek <dstlmrk@gmail.com>
2016-03-20 14:28:28 +0100

    script for local deployment

--- bin/local-createDatabase.sh
+++ bin/local-createDatabase.sh
@@ -1,5 +1,20 @@
+# creates new db schema
 if cat ../sql/createDb.sql | mysql; then
     echo -e "\e[32mDatabase is created.\033[0m"
 else
     echo -e "\e[31mDatabase is NOT created.\033[0m"
+fi
+
+# imports data from cald db
+if python ../catcher/importCaldData.py; then
+    echo -e "\e[32mData from cald db are imported.\033[0m"
+else
+    echo -e "\e[31mData from cald db are NOT imported.\033[0m"
+fi
+
+# creates init data
+if python ../catcher/createInitData.py; then
+    echo -e "\e[32mInit data are created.\033[0m"
+else
+    echo -e "\e[31mInit data are NOT created.\033[0m"
 fi
