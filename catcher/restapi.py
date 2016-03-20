9799b7e672c8d46c4ad93ffefd3083113b1580fb
dosmarek <dstlmrk@gmail.com>
2016-03-20 14:16:12 +0100

    logging sql queries

--- catcher/restapi.py
+++ catcher/restapi.py
@@ -8,6 +8,12 @@ from catcher import models
 from catcher import errors
 from catcher import middleware
 
+# print all queries to stderr
+import logging
+logger = logging.getLogger('peewee')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
 '''
 falcon.API instances are callable WSGI apps
 
@@ -23,21 +29,28 @@ api = falcon.API(middleware=[
 ])
 
 # resources are represented by long-lived class instances
-api.add_route('/club/{id}', resources.Club(models.Club, ['shortcut', 'city', 'country']))
-api.add_route('/clubs', resources.Clubs(models.Club))
+api.add_route('/club/{id}', r.Club(m.Club,['shortcut', 'city', 'country']))
+api.add_route('/clubs', r.Clubs(m.Club))
 
-api.add_route('/player/{id}', resources.Player(models.Player, ['firstname', 'lastname', 'nickname', 'number', 'ranking']))
-api.add_route('/players', resources.Players(models.Player))
+api.add_route('/player/{id}', r.Player(m.Player,
+    ['firstname', 'lastname', 'nickname', 'number', 'ranking']))
+api.add_route('/players', r.Players(m.Player))
 
-api.add_route('/division/{id}', resources.Division(models.Division, ['division']))
-api.add_route('/divisions', resources.Divisions(models.Division))
+api.add_route('/division/{id}', r.Division(m.Division, ['division']))
+api.add_route('/divisions', r.Divisions(m.Division))
 
-api.add_route('/team/{id}', resources.Division(models.Team, ['divisionId', 'degree']))
-api.add_route('/teams', resources.Divisions(models.Team))
+api.add_route('/team/{id}', r.Team(m.Team, ['divisionId', 'degree']))
+api.add_route('/teams', r.Teams(m.Team))
 
+# TODO: dodelat
+api.add_route('/tournament/{id}', r.Tournament(m.Tournament, []))
+api.add_route('/tournaments', r.Tournaments(m.Tournament))
+api.add_route('/createTournament', r.CreateTournament())
 
+# errors
 api.add_error_handler(Exception, errors.InternalServerError)
 api.add_error_handler(TypeError, errors.BadRequest)
 api.add_error_handler(KeyError, errors.BadRequest)
-api.add_error_handler(models.MySQLModel.DoesNotExist, errors.NotFound)
+api.add_error_handler(ValueError, errors.BadRequest)
+api.add_error_handler(m.MySQLModel.DoesNotExist, errors.NotFound)
 api.add_error_handler(AttributeError, errors.BadRequest)
